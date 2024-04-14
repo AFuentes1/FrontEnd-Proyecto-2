c0868a5c73c228a395944d391bbbe75567ec156c
AFuentes1 <anthonyfuentescalvo@gmail.com>
2024-04-14 14:58:07 -0600

    [ADD] Boton Consultar

--- Principal/Scripts.js
+++ Principal/Scripts.js
@@ -94,3 +94,71 @@ function mostrarTabla(data) {
     // Agregar la tabla al contenedor
     dataContainer.appendChild(table);
 }
+
+//BOTON Consulta
+document.addEventListener("DOMContentLoaded", function() {
+    // Leer la cookie para obtener userKey
+    var userKey = getCookie("userKey");
+    console.log("js2C", userKey);
+
+    // Verificar si la userKey está presente
+    if (!userKey) {
+        // La userKey no está presente, probablemente el usuario no ha iniciado sesión
+        console.log("No se encontró la userKey en la cookie");
+        // Puedes redirigir al usuario a la página de inicio de sesión u otra acción adecuada aquí
+        return;
+    }
+
+    // Obtener referencia a los elementos HTML
+    var filtroSelect = document.getElementById('filtroSelect');
+    var consultaInput = document.getElementById('consultaInput');
+    var consultaButton = document.getElementById('consultaButton');
+
+    // Asignar evento de clic al botón de consulta
+    consultaButton.addEventListener('click', function() {
+        // Obtener el valor del campo de texto y de la lista desplegable
+        var filtro = document.getElementById('consultaInput').value;
+        console.log(filtro);
+        var consulta = consultaInput.value;
+
+        // Verificar que ambos valores no estén vacíos antes de realizar la consulta
+        if (filtro && consulta) {
+            // Crear objeto de datos a enviar al servidor
+            var formData = {
+                key: userKey,
+                filtro: filtro, // Agregar el valor seleccionado en la lista desplegable
+                consulta: consulta // Agregar la consulta al objeto de datos
+            };
+
+            var jsonData = JSON.stringify(formData);
+            // Enviar solicitud al API
+            fetch('https://localhost:7081/Consulta', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json' // Incluye la userKey en el encabezado de autorización
+                },
+                body: jsonData // Envía la userKey, filtro y consulta al servidor
+            })
+            .then(response => {
+                console.log(response);
+                if (response.ok) {
+                    return response.json() // Devuelve los datos JSON de la respuesta
+                } else {
+                    throw new Error('La respuesta del servidor no es válida');
+                }
+            })
+            .then(data => {
+                // Llamar a la función para mostrar la tabla con los datos recibidos de la API
+                console.log(data);
+                mostrarTabla(data);
+            })
+            .catch(error => {
+                // Para manejar los errores 
+                console.error('Error:', error);
+            });
+        } else {
+            // Mostrar un mensaje de error si falta algún valor
+            console.error('Por favor ingrese un valor en el campo de texto y seleccione un filtro');
+        }
+    });
+});
